396c897c394ac3eb1bf8ccb5ecaee319b624654d
sogdian <tarssvisionkira@gmail.com>
2026-06-06 13:10:50 +0500

    zad1

---
+++ Cat_Kaigorodov/Cat.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_Kaigorodov
+{
+    class Cat
+    {
+        private string name;
+        private double ves;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                bool OnlyLetters = true;
+
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+
+        public double Ves
+        {
+            get
+            {
+                return ves;
+            }
+
+            set
+            {
+                if (value <= 0 || value > 20)
+                {
+                    Console.WriteLine($"{value} - неправильный вес!!!");
+                }
+                else
+                {
+                    ves = value;
+                }
+            }
+        }
+
+        public Cat(string CatName)
+        {
+            Name = CatName;
+        }
+
+        public void golos()
+        {
+            Console.WriteLine($"{name}: МЯЯЯУ!");
+        }
+
+    }
+}
---
+++ Cat_Kaigorodov/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_Kaigorodov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество котов: ");
+            int Count = Convert.ToInt32(Console.ReadLine());
+
+            Cat[] cats = new Cat[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine($"----Кот №{i + 1}----");
+
+                cats[i] = new Cat("");
+
+                Console.Write($"Введите имя кота: ");
+                cats[i].Name = Console.ReadLine();
+
+                Console.Write($"Введите вес кота (0 < вес <= 20): ");
+                cats[i].Ves = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("---- Результат ----");
+            
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine($"Кот {i + 1}: ");
+                cats[i].golos();
+                Console.WriteLine($"Вес: {cats[i].Ves} кг");
+            }
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
